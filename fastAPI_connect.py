4a1dbc55481b3433def3c69a68955fcb723f1718
yugamax <ayp14111@gmail.com>
2025-03-19 20:29:47 +0530

    Faster response version

--- fastAPI_connect.py
+++ fastAPI_connect.py
@@ -112,4 +112,4 @@ app.include_router(sk_router, prefix="/image")
 app.include_router(disease_router, prefix="/disease")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
